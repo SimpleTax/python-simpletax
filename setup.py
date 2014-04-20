e0d1fb56f4db0ff35fae9fbf6a20b65ef8b83877
Celso Pinto <celso@gosimpletax.com>
2014-04-20 01:16:09 +0100

    further fixes to setup.py

--- setup.py
+++ setup.py
@@ -6,11 +6,7 @@ setup(
     author='Celso Pinto',
     author_email='celso@gosimpletax.com',
     packages=['simpletax'],
-    url='http://pypi.python.org/pypi/python-simpletax/',
+    url='http://github.org/leftfield/python-simpletax/',
     license='LICENSE.txt',
     description='Python API client to gosimpletax.com',
-    long_description=open('README.markdown').read(),
-    install_requires=[
-        "requests-oauthlib == 0.3.2",
-    ],
 )
